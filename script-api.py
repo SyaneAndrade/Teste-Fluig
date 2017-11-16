343e2267b2098be628b7dad7f3310849c6603dd6
Jessiane Gomes Andrade <syane_andrade@outlook.com>
2017-11-16 01:57:24 -0200

    Tirei import não utilizado

--- script-api.py
+++ script-api.py
@@ -1,14 +1,13 @@
-import json
 import requests
 import pandas as pd
 
-#Faz a requisição get
+#Faz a requisição get, caseo necessite poderá mudar a url do get
 def get():
     return requests.get('https://vaga-bi-fluig.herokuapp.com/api/automation/initial-test/')
 
 #Convert o json retornado no content da requisição em um arquivo excel
 def convert_Json_xlsx(response):
-    pd.read_json(response.content).to_excel("json_test.xlsx")
+    pd.read_json(response.content).to_excel("json_excel.xlsx")
 
 def main():
     response = get()
